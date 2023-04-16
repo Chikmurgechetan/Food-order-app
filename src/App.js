4590808076a71f7e039386b13790365faf5c3987
Chikmurgechetan <chikmurgechetan@gmail.com>
2023-04-16 15:43:21 +0530

    Adding Meals Summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Summery from "./components/Meals/Summary";
 function App() {
   return (
     <>
       <Header/>
+      <Summery/>
     </>
   );
 }
